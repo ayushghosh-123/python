e615ed2ea980cb640a1c624a31efcae830ea325c
Ayushghosh-123 <ghoshayush910@gmail.com>
2025-11-05 01:55:48 +0530

    multiprocessing running

--- udemy_course/Python_udemy/12_thread_concurrency/09_process_one.py
+++ udemy_course/Python_udemy/12_thread_concurrency/09_process_one.py
@@ -4,8 +4,14 @@ import time
 def cpu_heavy():
     print(f'crunching some numbers...')
     total = 0
-    for i in range(10**7):
+    for i in range(10**8):
         total +=i
     print("Done ")
 
 start = time.time()
+threads = [threading.Thread(target=cpu_heavy) for _ in range(2)]
+
+[t.start() for t in threads]
+[t.join() for t in threads]
+
+print(f'Time taken:{time.time() - start:.2f} seconds')
